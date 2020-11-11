f643ca1bdca6e8afdb1bd7150e548ea1487c7839
Paulo Júnior <paulo.junior@ccc.ufcg.edu.br>
2020-11-11 19:49:02 -0300

    Delete subjects_controller.py

--- controllers/subjects_controller.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from flask_cors import cross_origin
-from flask import jsonify
-from . import routes
-
-@routes.route("/disciplinas")
-@cross_origin()
-def get_rates():
-  return jsonify({"ok": "true"})
