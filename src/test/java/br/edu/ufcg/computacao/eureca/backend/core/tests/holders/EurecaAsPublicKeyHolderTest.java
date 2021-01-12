4ba40baa5283551754d90cbc323269310084de8a
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-01-12 18:58:28 -0300

    Added test classes for util package.

--- src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/holders/EurecaAsPublicKeyHolderTest.java
+++ src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/holders/EurecaAsPublicKeyHolderTest.java
@@ -5,18 +5,26 @@ import br.edu.ufcg.computacao.eureca.common.exceptions.EurecaException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.security.interfaces.RSAPublicKey;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EurecaAsPublicKeyHolderTest {
 
+    @Autowired
     private RSAPublicKey asPublicKey;
 
+    @Autowired
     private EurecaAsPublicKeyHolder instance;
 
     @Before
     public void setUp() throws EurecaException {
         instance = EurecaAsPublicKeyHolder.getInstance();
+        asPublicKey = instance.getAsPublicKey();
     }
 
     @Test
@@ -25,8 +33,6 @@ public class EurecaAsPublicKeyHolderTest {
     }
 
     @Test
-    public void getAsPublicKeyTest() {
-
-    }
+    public void getAsPublicKeyTest() throws EurecaException { }
 
 }
