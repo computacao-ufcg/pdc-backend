0982542f4c59883fddf324c7ec945683dd80f9c7
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-01-21 12:58:55 -0300

    Trying mock of MetricCalculator constructor behavior.

--- src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/util/MetricsCalculatorTest.java
+++ src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/util/MetricsCalculatorTest.java
@@ -1,42 +1,55 @@
 package br.edu.ufcg.computacao.eureca.backend.core.tests.util;
 
+import br.edu.ufcg.computacao.eureca.backend.core.dao.DataAccessFacade;
 import br.edu.ufcg.computacao.eureca.backend.core.dao.scsvfiles.mapentries.Registration;
+import br.edu.ufcg.computacao.eureca.backend.core.models.AttemptsSummary;
 import br.edu.ufcg.computacao.eureca.backend.core.util.MetricsCalculator;
-import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.Map;
+import java.util.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DataAccessFacade.class)
 public class MetricsCalculatorTest {
 
-    @MockBean
+    @InjectMocks
     private static MetricsCalculator instance;
 
-    @Autowired
+    @Mock
     private Map<Registration, Integer> attemptsMap;
-
-    @LocalServerPort
-    private int port;
 
     @Before
     public void setUp() {
-        instance = MetricsCalculator.getInstance();
+        attemptsMap = new HashMap<>();
+        Registration reg1 = new Registration("123456789");
+        Registration reg2 = new Registration("323131331");
+        Registration reg3 = new Registration("355125512");
+        attemptsMap.put(reg1, 24);
+        attemptsMap.put(reg2, 20);
+        attemptsMap.put(reg3, 18);
     }
 
     @Test
     public void getInstanceTest() {
-        Assertions.assertThat(instance).isNotEqualTo(null);
+        mockInstance();
+        instance = MetricsCalculator.getInstance();
+    }
+
+    public void mockInstance() {
+        DataAccessFacade dataAccessFacade = mock(DataAccessFacade.class);
+        Mockito.when(dataAccessFacade.getAttemptsSummary()).thenReturn((Collection<AttemptsSummary>) attemptsMap);
+        PowerMockito.mockStatic(DataAccessFacade.class);
+//        BDDMockito.given(DataAccessFacade.getInstance()).willReturn(dataAccessFacadeHolder);
     }
 
 }
