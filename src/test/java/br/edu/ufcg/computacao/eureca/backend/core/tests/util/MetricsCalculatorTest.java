235630295fb24855d9616646ec71f3bfb0a11fd9
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2021-01-23 13:53:43 -0300

    MetricsCalculatorTest class completed.

--- src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/util/MetricsCalculatorTest.java
+++ src/test/java/br/edu/ufcg/computacao/eureca/backend/core/tests/util/MetricsCalculatorTest.java
@@ -1,8 +1,13 @@
 package br.edu.ufcg.computacao.eureca.backend.core.tests.util;
 
 import br.edu.ufcg.computacao.eureca.backend.core.dao.DataAccessFacade;
+import br.edu.ufcg.computacao.eureca.backend.core.dao.scsvfiles.mapentries.CpfRegistration;
+import br.edu.ufcg.computacao.eureca.backend.core.dao.scsvfiles.mapentries.StudentData;
 import br.edu.ufcg.computacao.eureca.backend.core.holders.DataAccessFacadeHolder;
+import br.edu.ufcg.computacao.eureca.backend.core.models.Metrics;
+import br.edu.ufcg.computacao.eureca.backend.core.models.Student;
 import br.edu.ufcg.computacao.eureca.backend.core.util.MetricsCalculator;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +15,7 @@ import org.mockito.BDDMockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.mock;
 
@@ -25,12 +31,13 @@ public class MetricsCalculatorTest {
         this.metricsCalculator = MetricsCalculator.getInstance();
     }
 
-    //Test case: Call the getInstance method and tests a successfully return.
+    // Test case: Call the getInstance method and tests a successfully return.
     @Test
     public void getInstanceTest() {
-        assertNotEquals(null,this.metricsCalculator);
+        assertNotEquals(this.metricsCalculator, null);
     }
 
+    // mock method from the DataAccessFacadeHolder class, that simulates the behavior of a real object.
     public void mockDataAccessFacadeHolder() {
         DataAccessFacadeHolder dataAccessFacadeHolder = DataAccessFacadeHolder.getInstance();
         DataAccessFacade dataAccessFacade = mock(DataAccessFacade.class);
@@ -38,4 +45,30 @@ public class MetricsCalculatorTest {
         PowerMockito.mockStatic(DataAccessFacadeHolder.class);
         BDDMockito.given(DataAccessFacadeHolder.getInstance()).willReturn(dataAccessFacadeHolder);
     }
+
+    // checks the behavior of computeMetrics method, and whether it returns an object of type Metrics.
+    @Test
+    public void computeMetricsWithValidStudentTest() {
+        // Student object creation.
+        String registrationNumber = "12346533354";
+        CpfRegistration cpfRegistrationFake = new CpfRegistration("+55", registrationNumber);
+        StudentData studentDataFake = new StudentData("x", "x", "x", "x", "x",
+                "x", "x", "x", "Inativo (GRADUADO 2011.2)",
+                "VESTIBULAR 2007.2", "x", "x", "x",
+                "x", 0,0,0,
+                0,0,0,0,
+                0,0,0,0,0,
+                0,0,0);
+
+        Student student = new Student(cpfRegistrationFake, studentDataFake);
+
+        Assert.assertEquals(metricsCalculator.computeMetrics(student) instanceof Metrics, true);
+    }
+
+    // checks the behavior of computeMetrics when the student parameter is null.
+    @Test
+    public void computeMetricsWithNullStudentParameter() throws NullPointerException {
+        Assert.assertEquals(metricsCalculator.computeMetrics(null), null);
+    }
+
 }
